063c922c8508be2c0f15cbfcd150db19f93f2e63
Dramiel Leayal <leayal@users.noreply.github.com>
2019-09-03 00:03:28 +0700

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -6,4 +6,6 @@ dotnet publish ./BlueVowsLauncher/BlueVowsLauncher.csproj -c Release --no-self-c
 dotnet publish ./BlueVowsLauncher/BlueVowsLauncher.csproj -c Release --no-self-contained -r win10-x64 /p:PublishReadyToRun=true -o ./Build/Compact/win10-x64
 dotnet publish ./BlueVowsLauncher/BlueVowsLauncher.csproj -c Release --no-self-contained -r win7-x86 /p:PublishReadyToRun=true -o ./Build/Compact/win7-x86
 
+dotnet publish ./BlueVowsLauncher/BlueVowsLauncher.csproj -c Release --no-self-contained /p:PublishReadyToRun=true -o ./Build/Compact/Portable
+
 REM dotnet publish ./BlueVowsLauncher/BlueVowsLauncher.csproj -c Release --self-contained -r win10-x64 /p:PublishReadyToRun=true -o ./Build/win10-x64
